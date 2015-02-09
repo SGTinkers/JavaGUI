bfaac6a1466362527df5148a6bfb0345763210ba
muhdmirzamz <muhdmirzamz@gmail.com>
2015-02-09 14:24:14 +0800

    Calendar GUI done

--- src/javaGUI/calendar/Calendar.java
+++ src/javaGUI/calendar/Calendar.java
@@ -1,6 +1,14 @@
-package peoplePal.ui;
+/*
+ * File: Calendar.java
+ * Description: a calendar picker GUI
+ * Author: Muhd Mirza
+ * Date created: 5 Feb 2015
+ */
+
+package javaGUI.calendar;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -9,8 +17,10 @@ import java.awt.event.WindowEvent;
 import java.util.GregorianCalendar;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
@@ -19,13 +29,10 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
-import peoplePal.entity.User;
-import peoplePal.util.Button;
-
-public class LoginCalendarPanel extends MasterLoginPanel {
+public class Calendar extends JPanel {
 	
-	private User user;
-	private String birthDate;
+	private static JFrame frame;
+	private JFrame miniFrame;
 	
 	private String[] dayString;
 	private String[] monthString;
@@ -42,8 +49,10 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 	
 	private DefaultTableModel dtm;
 	private JTable calendarTable;
+	private int tableValue;
+	private String dateString;
 	
-	private Button done;
+	private JButton done;
 	
 	/*
 	 * calendar workflow on first loop:
@@ -59,10 +68,27 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 	 * - set up the gui table
 	 */
 	
-	public LoginCalendarPanel(final JFrame frame, final User user) {
-		super(frame);
+	public Calendar() {
+		super();
 		
-		this.user = user;
+		/*
+		 * declarations and initialisations of variables
+		 */
+		
+		/*
+		 * passing the data here
+		 * dealing with the actual default table model messes things up
+		 */
+		tableValue = 0;
+		
+		/*
+		 * Using two variables to keep track of the days and to facilitate the loop
+		 * actualDays - increment this to simulate days
+		 * totalDays - initialised at a later stage, this holds the total days in a selected month
+		 *  
+		 * actualDays goes up, totalDays goes down
+		 */
+		actualDays = 1;
 		
 		dayString = new String[7];
 		dayString[0] = "Sunday";
@@ -87,11 +113,21 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 		monthString[10] = "November";
 		monthString[11] = "December";
 		
-		gc = new GregorianCalendar();
+		/*
+		 * using only GregorianCalendar.YEAR tends to up your system timing by one
+		 * same as for GregorianCalendar.MONTH
+		 * calling the methods by object is better 
+		 */ 
+ 		gc = new GregorianCalendar();
 		year = gc.get(GregorianCalendar.YEAR);
 		month = gc.get(GregorianCalendar.MONTH);
 		
+		
+		/*
+		 * set up the GUIs
+		 */
 		monthComboBox = new JComboBox(new DefaultComboBoxModel(monthString));
+		monthComboBox.setSelectedIndex(month); // set default to system month
 		monthComboBox.setBounds(10, 10, 100, 40);
 		monthComboBox.addActionListener(new ActionListener() {
 
@@ -156,7 +192,7 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 		add(monthComboBox);
 		
 		yearComboBox = new JComboBox();
-		for (int i = year - 80; i < year + 20; i++) {
+		for (int i = year - 80; i < year + 20; i++) { // by doing this, you are able to specify range of the JComboBox
 			yearComboBox.addItem(i);
 		}
 		yearComboBox.setSelectedIndex(80); // year - 80   -------- year --------- year + 20
@@ -174,9 +210,6 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 		});
 		add(yearComboBox);
 		
-		// set days
-		actualDays = 1;
-		
 		// set table model
 		dtm = new DefaultTableModel() {
 			public boolean isCellEditable(int row, int column) {
@@ -186,41 +219,45 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 		dtm.setColumnCount(7);
 		dtm.setRowCount(7);
 		
+		// populate table's first row with days 
 		for (int i = 0; i < 7; i++) {
 			dtm.setValueAt(String.valueOf(dayString[i]), 0, i);
 		}
 		
-		// method call here to facilitate first loop
+		/*
+		 *  method call here to facilitate first loop
+		 *  do not put the contents of this method in the constructor
+		 *  the program will loop and use the same table model 
+		 *  it will either crash or give you inaccurate results
+		 */
 		fillTable(year, month);
 		
-		done = new Button("Done", Color.BLACK, 420, 400);
-		done.getButton().addActionListener(new ActionListener() {
+		done = new JButton("Done"); 
+		done.setFont(new Font("Tahoma", Font.PLAIN, 12));		
+		done.setForeground(Color.BLACK);
+		done.setBounds(420, 400, 50, 50);
+		done.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				System.out.println("jkcnakcnac");
-				
-				int row = calendarTable.getSelectedRow();
-				int column = calendarTable.getSelectedColumn();
-				int value = (int)calendarTable.getValueAt(row, column);
-				
 				if (true) {
-					System.out.println(value + "-" + (gc.get(GregorianCalendar.MONTH) + 1) + "-" + gc.get(GregorianCalendar.YEAR));
+					System.out.println(tableValue + "-" + (gc.get(GregorianCalendar.MONTH) + 1) + "-" + gc.get(GregorianCalendar.YEAR));
 				}
 				
-				birthDate = value + "-" + (gc.get(GregorianCalendar.MONTH) + 1) + "-" + gc.get(GregorianCalendar.YEAR); 
-				user.setBirthdate(birthDate);
+				JOptionPane.showMessageDialog(miniFrame, "You chose " + dateString +
+											 (gc.get(GregorianCalendar.MONTH) + 1) + "-" 
+											  + gc.get(GregorianCalendar.YEAR));
 				
+				// close window after clicking ok in alert box
 				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 			}
 			
 		});
-		add(done.getButton());
+		add(done);
 	}
 	
 	public void fillTable(final int year, final int month) {
-		
+
 		// reset table
 		for (int i = 0; i < 7; i++) {
 			dtm.setValueAt(null, 1, i);
@@ -246,7 +283,11 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 			dtm.setValueAt(null, 6, i);
 		}
 		
-		// initialise a new GregorianCalendar object
+		/*
+		 * initialise a new GregorianCalendar object with parameters
+		 * these parameters specify the modified / selected year, month and first day
+		 * same thing, you need to do this so the program will not reuse the old table model
+		 */
 		gc = new GregorianCalendar(year, month, 1);
 		firstDayOfMonth = gc.get(GregorianCalendar.DAY_OF_WEEK);
 		totalDays = gc.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
@@ -305,12 +346,17 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
 				JTable temp = (JTable) e.getSource();
 				int row = temp.getSelectedRow();
 				int column = temp.getSelectedColumn();
-				int value = (int)temp.getValueAt(row, column);
+				tableValue = (int) temp.getValueAt(row, column);
 				
-				System.out.println(value + "-" + (gc.get(GregorianCalendar.MONTH) + 1) + "-" + gc.get(GregorianCalendar.YEAR));
+				dateString = String.valueOf(tableValue);
+				
+				if (row >= 1) {
+					System.out.println(tableValue + "-" + (gc.get(GregorianCalendar.MONTH) + 1) + "-" + gc.get(GregorianCalendar.YEAR));
+				}
 			}
 
 			@Override
@@ -345,5 +391,16 @@ public class LoginCalendarPanel extends MasterLoginPanel {
 		calendarTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		calendarTable.setCellSelectionEnabled(true);
 		add(calendarTable);
+	}
+	
+	public static void main(String[] args) {
+		frame = new JFrame();
+		
+		JPanel calendar = new Calendar();
+		
+		frame.setContentPane(calendar);
+		frame.setVisible(true);
+		frame.setBounds(10, 10, 600, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
